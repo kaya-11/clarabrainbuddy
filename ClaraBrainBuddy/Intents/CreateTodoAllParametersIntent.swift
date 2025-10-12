b992008da564cd4ba5b16a188028026c5b5cc328
kaya-11 <129401117+kaja-11@users.noreply.github.com>
2025-10-12 09:53:40 +0200

    Added new intent creating task with all params

---
+++ ClaraBrainBuddy/Intents/CreateTodoAllParametersIntent.swift
@@ -0,0 +1,62 @@
+//
+//  Intents/CreateTodoIntent.swift
+//  ClaraBrainBuddy
+//
+//  Created by Karen on 30.09.25.
+//
+
+import AppIntents
+
+struct CreateTodoAllParamsIntent: AppIntent {
+    static var title: LocalizedStringResource = "app.intent.createtodo.allparams.title"
+    static var description = IntentDescription("app.intent.createtodo.allparams.description")
+
+    @Parameter(
+        title: LocalizedStringResource("app.intent.createtodo.param.title")
+    )
+    var title: String
+    
+    @Parameter(
+        title: LocalizedStringResource("app.intent.createtodo.param.description")
+    )
+    var description: String?
+
+    @Parameter(
+        title: LocalizedStringResource("app.intent.createtodo.param.duedate")
+    )
+    var dueDate: Date?
+
+    @Parameter(
+        title: LocalizedStringResource("app.intent.createtodo.param.estimatedtime")
+    )
+    var estimatedTime: Int?
+    
+    @MainActor
+    func perform() async throws -> some IntentResult {
+        if title.isEmpty {
+            return .result()
+        }
+        
+        let intentDueDate = dueDate ?? Date()
+        
+        let isDueToday = Calendar.current.isDate(intentDueDate, inSameDayAs: Date())
+        
+        if isDueToday {
+            TodoViewModel.shared.addNewTodoForToday(
+                title: title,
+                details: description ?? "",
+                estimatedTime: Int64(estimatedTime!),
+                recurringTask: nil
+            )
+        } else {
+            TodoViewModel.shared.addTodo(
+                title: title,
+                details: description ?? "",
+                dueDate: intentDueDate,
+                estimatedTime: Int64(estimatedTime!)
+            )
+        }
+        
+        return .result()
+    }
+}
