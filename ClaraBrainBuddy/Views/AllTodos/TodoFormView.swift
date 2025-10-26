471b72222179ce9e00ba80e07f96d11a9b9262b9
kaya-11 <129401117+kaja-11@users.noreply.github.com>
2025-10-26 12:51:41 +0100

    Removed unnecessary comments

--- ClaraBrainBuddy/Views/AllTodos/TodoFormView.swift
+++ ClaraBrainBuddy/Views/AllTodos/TodoFormView.swift
@@ -13,8 +13,6 @@ struct TodoFormView: View {
     
     @ObservedObject var todoViewModel: TodoViewModel
     
-    //var addDays : Int = 14
-    
     // If nil → Add Mode | If non-nil → Edit Mode
     var existingTodo: Todo?
 
