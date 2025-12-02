a1e4a831421023e5e661e3d6e2dc9e1740b552f6
kaya-11 <129401117+kaja-11@users.noreply.github.com>
2025-12-02 23:10:37 +0100

    Adapted Priority View to also drag and drop between the categories.

--- ClaraBrainBuddy/Views/common/TodoListEntrySimpleView.swift
+++ ClaraBrainBuddy/Views/common/TodoListEntrySimpleView.swift
@@ -12,16 +12,8 @@ struct TodoListEntrySimpleView: View {
 
     var body: some View {
         VStack(alignment: .leading, spacing: 4) {
-            HStack(alignment: .firstTextBaseline) {
-                Text(todo.title)
-                    .font(Font.app.listItem)
-                Spacer()
-                if todo.energyImpact < 0 {
-                    Image(systemName: "battery.50")
-                } else if todo.energyImpact > 0 {
-                    Image(systemName: "battery.100")
-                }
-            }
+            Text(todo.title)
+                .font(Font.app.listItem)
             Text("\(todo.dueDate, formatter: StyleUtils.dateFormatter)")
                     .font(Font.app.tiny)
                     .foregroundColor(.secondary)
